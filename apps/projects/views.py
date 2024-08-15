ae5d2dd693e49810f1a918747eb4435a792f1750
Zane Myers <zane@canopyteam.org>
2024-08-15 16:37:10 -0500

    Model Forms

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from apps.projects.models import Project
-
-projectsList = [
-    {"id": "1", "title": "E-commerce Website", "description": "This is an e-commerce website."},
-    {"id": "2", "title": "Portfolio Website", "description": "I built out my portfolio."},
-    {"id": "3", "title": "Social Network", "description": "Awesome open source project!"},
-]
+from apps.projects.forms import ProjectForm
 
 
 def projects(request):
@@ -17,3 +12,9 @@ def projects(request):
 def project(request, pk):
     project_obj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': project_obj})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context=context)
