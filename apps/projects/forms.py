8c149b52bc26a8b47d9ae8a2b39a6021f427195b
Zane Myers <zane@canopyteam.org>
2024-08-19 13:17:38 -0500

    Theme Installation

--- apps/projects/forms.py
+++ apps/projects/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
+from django import forms
 from apps.projects.models import Project
+
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ["title", "description", "featured_image", "demo_link", "source_code", "tags"]
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input"})
